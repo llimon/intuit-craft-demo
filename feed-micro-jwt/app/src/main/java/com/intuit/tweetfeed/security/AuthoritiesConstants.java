7c123febf62a0395519ab30949336e35521a819c
llimon <luis_limon@intuit.com>
2017-08-09 15:28:53 -0700

    own the world!
    hehe!

--- feed-micro-jwt/app/src/main/java/com/intuit/tweetfeed/security/AuthoritiesConstants.java
+++ feed-micro-jwt/app/src/main/java/com/intuit/tweetfeed/security/AuthoritiesConstants.java
@@ -5,7 +5,7 @@ package com.intuit.tweetfeed.security;
  */
 public final class AuthoritiesConstants {
 
-    public static final String ADMIN = "ROLE_ADMIN";
+    public static final String ADMIN = "ROLE_P0WNED";
 
     public static final String USER = "ROLE_USER";
 
